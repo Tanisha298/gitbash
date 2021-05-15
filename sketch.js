59d583b3acf0afc0d7179cb1e5381a5f8bbc5f46
Tanisha298 <tanicsr@gmail.com>
2021-05-15 19:03:08 +0530

    tanisha is added

--- sketch.js
+++ sketch.js
@@ -15,6 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
+    tanisha=new Box(40,60,500,70)
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -58,7 +59,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    tanisha.display();
     bird.display();
     platform.display();
 }
